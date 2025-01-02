9db287e6e5d7eb9350ba60f65e36686b719d554c
Gamerabbit <jordapaul16@gmail.com>
2025-01-02 13:43:58 +0100

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pick-a-Pall
+//
+//  Created by Paul Jorda on 02/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pick_a_PallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PallApp.swift
+//  Pick-a-Pall
+//
+//  Created by Paul Jorda on 02/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
